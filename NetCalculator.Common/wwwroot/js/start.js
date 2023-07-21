d02011fe0e04f32c8011706ee4c2d84adcee9c22
josago97 <josago2009@hotmail.es>
2023-07-21 20:09:41 +0200

    Fixed max width in VLSM Page

--- NetCalculator.Common/wwwroot/js/start.js
+++ NetCalculator.Common/wwwroot/js/start.js
@@ -1,10 +1,6 @@
 import { changeTheme } from "./app.js"
 
-
-console.log(4);
-
 // If dark mode is enbaled, then it activates dark mode for loading page
-if (localStorage.darkMode || window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    console.log(6);
+if (localStorage.darkMode === "true" || window.matchMedia("(prefers-color-scheme: dark)").matches) {
     changeTheme(true);
 }
